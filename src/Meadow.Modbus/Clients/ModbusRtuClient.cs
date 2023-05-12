79f6073d34b91808142c89d022bb7421d23707a1
Paul <paul.farrand@primetss.com.au>
2023-05-12 12:53:44 +0930

    Added WriteMultipleCoils() feature
    Added WriteMultipleCoils() method to ModbusRTU Serial Client. Tested on physical RS485 Modbus RTU IO Module.
    Added Unit Test for WriteMultipleCoils() to check Modbus message generation & expected return. Also confirmed this using this online message generation tool https://rapidscada.net/modbus/
    
    Using this a protocol reference https://www.modbustools.com/modbus.html#function15
    
    Did not add to TCP Client as found issues with message construction for TCP message that require further work.

--- src/Meadow.Modbus/Clients/ModbusRtuClient.cs
+++ src/Meadow.Modbus/Clients/ModbusRtuClient.cs
@@ -188,7 +188,9 @@ namespace Meadow.Modbus
 
             switch (function)
             {
-                case ModbusFunction.WriteMultipleCoils:
+                case ModbusFunction.WriteMultipleCoils: // Not implemented yet
+                    message = new byte[4 + data.Length + 2]; // header + length + data + crc
+                    break;
                 case ModbusFunction.WriteMultipleRegisters:
                     message = new byte[4 + data.Length + 5]; // header + length + data + crc
                     break;
@@ -204,7 +206,8 @@ namespace Meadow.Modbus
 
             switch (function)
             {
-                case ModbusFunction.WriteMultipleCoils:
+                case ModbusFunction.WriteMultipleCoils: // Not implemented yet
+                    break;
                 case ModbusFunction.WriteMultipleRegisters:
                     var registers = (ushort)(data.Length / 2);
                     message[4] = (byte)(registers >> 8);
